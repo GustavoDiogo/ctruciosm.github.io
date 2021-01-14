c059caeee207df328408b157e8d5c26fc310de82
ctruciosm <ctrucios@gmail.com>
2021-01-14 08:42:27 -0300

    aula04

---
+++ MRP/MRLM_Aula4.R
@@ -0,0 +1,44 @@
+#################################
+####  MRLM - Aula 04
+#################################
+library(car)
+library(corrplot)
+
+
+### Carregando os dados
+WAGE1 = read.table("./DadosMRP/wage1.txt")
+colnames(WAGE1) = c("wage","educ","exper","tenure",
+                    "nonwhite","female","married",
+                    "numdep","smsa","northcen","south",
+                    "west","construc","ndurman","trcommpu",
+                    "trade","services","profserv","profocc",
+                    "clerocc","servocc","lwage","expersq","tenursq")
+
+
+### Ajustando o modelo
+modelo1 = lm(log(wage) ~ educ, data = WAGE1)
+modelo1
+
+
+modelo3 = lm(log(wage) ~ educ + exper + tenure, data = WAGE1)
+modelo3
+
+modelof = lm(log(wage) ~ ., data = WAGE1)
+modelof
+
+### Mais informação do modelo
+
+summary(modelo1)
+
+summary(modelo3)
+
+summary(modelof)
+
+
+### Multicolinearidade
+
+corrplot(cor(WAGE1))
+vif(modelof)
+
+
+
